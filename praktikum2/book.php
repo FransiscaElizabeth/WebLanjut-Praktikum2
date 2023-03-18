9d77f78a4abe9dc558422c97bb56fbde49aed457
Kristiani Nainggolan <105917024+KristianiNaingg@users.noreply.github.com>
2023-03-18 22:26:04 +0700

    Update book.php

--- praktikum2/book.php
+++ praktikum2/book.php
@@ -1,36 +1,39 @@
 <?php
-    $link = new PDO('mysql host:localhost; dbname = pwl20222', 'root', '');
-    $link -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $link -> setAttribute(PDO::ATTR_AUTOCOMMIT, false);
-    $query = 'SELECT book.isbn, book.title, book.author, book.publisher, book.publish_year, genre.name FROM book JOIN genre ON book.genre_id=genre.id';
-    $stmt = $link->prepare($query);
-    $stmt->execute();
-    $result = $stmt->fetchAll();
-    $link = null
+$link = new PDO(dsn:'mysql:host=localhost;dbname=pwl20222', username:'root', password:'');
+$link->setAttribute(attribute: PDO :: ATTR_ERRMODE, value:PDO :: ERRMODE_EXCEPTION);
+$link->setAttribute(attribute: PDO :: ATTR_AUTOCOMMIT,value:false);
+$query = 'SELECT isbn, title,author,publisher,publisher_year,short_description, cover FROM book';
+$stmt = $link->prepare($query);
+$stmt-> execute();
+$result = $stmt ->fetchAll();
+$link= null;
 ?>
 <table>
     <thead>
+    <tr>
         <th>ISBN</th>
-        <th>Title</th>
-        <th>ISBN</th>
-        <th>Title</th>
-        <th>ISBN</th>
-        <th>Title</th>
+        <th>TITLE</th>
+        <th>AUTHOR</th>
+        <th>PUBLISHER</th>
+        <th>PUBLISHER YEAR</th>
+        <th>SHORT DESCRIPTION</th>
+        <th>COVER</th>
+
+    </tr>
     </thead>
     <tbody>
     <?php
     foreach ($result as $book){
         echo '<tr>';
-        echo '<td>' . $book['isbn'] . '<td>';
-        echo '<td>' . $book['title'] . '<td>';
-        echo '<td>' . $book['author'] . '<td>';
-        echo '<td>' . $book['publisher'] . '<td>';
-        echo '<td>' . $book['publish_year'] . '<td>';
-    }
-    foreach ($result as $genre){
-        echo '<tr>';
-        echo '<td>' . $genre['name'] . '<td>';
+        echo '<td>'. $book ['isbn']. '</td>';
+        echo '<td>'. $book ['title']. '</td>';
+        echo '<td>'. $book ['author']. '</td>';
+        echo '<td>'. $book ['publisher']. '</td>';
+        echo '<td>'. $book ['publisher_year']. '</td>';
+        echo '<td>'. $book ['short_description']. '</td>';
+        echo '</tr>';
     }
     ?>
     </tbody>
+
 </table>
